0468b2b7d440109544cf61b263c76898f703cf21
JoForsman <johanna.m.forsman@gmail.com>
2025-06-26 07:59:51 +0200

    step 2 completed

--- code/script.js
+++ code/script.js
@@ -14,6 +14,27 @@ alert(
 
 // Step 2 - Food choice
 // Your code goes here
+let order = prompt("What would you like to eat today?\n\nToday we serve:\n1. Pizza\n2. Pasta\n3. Salad\n\nPlease enter a number to order!")
+
+let foodChoice;
+
+if (order === "1") {
+  foodChoice = "pizza";
+}
+else if (order === "2") {
+  foodChoice = "pasta";
+}
+
+else if (order === "3") {
+  foodChoice = "salad";
+}
+
+else {
+  foodChoice = prompt("Please enter a number between 1 and 3 to order.\n1. Pizza\n2. Pasta\n3. Salad")
+}
+
+alert(`So you're in the mood for ${foodChoice} today? Great choice!`
+)
 
 // Step 3 - Subtype choice
 // Your code goes here
